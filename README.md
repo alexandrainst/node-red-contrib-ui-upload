739cac4fd2aff28561ce0a40386ade4dde97756f
Alexandre Alapetite <alexandre.alapetite@alexandra.dk>
2020-07-02 23:54:16 +0200

    Implement backpressure
    And refactoring

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # node-red-contrib-ui-upload
 
-[Node-RED Dashboard](https://github.com/node-red/node-red-dashboard) UI widget node for **uploading** a file content by Socket.io streaming.
+[Node-RED Dashboard](https://github.com/node-red/node-red-dashboard) UI widget node for **uploading** a file content by WebSocket (Socket.io) streaming.
 
-Supports: browse for file or drag & drop; pause & resume, replay; custom chunk size.
+Supports: browse for file or drag & drop; pause & resume, replay; custom chunk size; backpressure.
 
 Screenshot in the Node-RED Dashboard:
 
@@ -15,6 +15,20 @@ In a Node-RED flow, this *Upload node* can advantageously be connected to some s
 Example: [flows.json](doc/flows.json)
 
 ![Node-RED flow](doc/flow.png)
+
+## Backpressure
+
+This node supports *backpressure* / *flow control*:
+it can wait for a *tick* before uploading the next chunk of data,
+to make sure the rest of your Node-RED flow is ready to process more data, instead of risking an out-of-memory condition.
+
+For that, just send `{ payload: { tick: true }}` to the node for triggering the upload of the next chunk of data.
+
+By default, in absence of wired input on this node, a tick will be automatically generated upon full reception of a chunk of data to trigger the upload of the next one.
+
+This backpressure mechanism also gives time to other nodes sending messages on the same shared WebSocket and avoids disconnections when uploading large files.
+
+## Credits
 
 License: [Apache 2.0](LICENSE.md), 2020.
 
--- ui-upload.js
+++ ui-upload.js
@@ -1,6 +1,6 @@
-module.exports = function (RED) {
-
-	const HTML = String.raw`
+function html(config) {
+	const jsonConfig = JSON.stringify(config);
+	return String.raw`
 <style>
 .ui-upload {
 	height: 100%;
@@ -31,6 +31,7 @@ module.exports = function (RED) {
 </style>
 
 <div id="ui-upload-{{unique}}" class="ui-upload"
+	ng-init='init(` + jsonConfig + `)'
 	ng-on-dragleave="ondragleave($event)" ng-on-dragenter="ondragenter($event)"
 	ng-on-dragover="ondragover($event)" ng-on-drop="ondrop($event)">
 	<p>{{title}}</p>
@@ -40,54 +41,47 @@ module.exports = function (RED) {
 	<button class="stop" ng-click="stopClick($event)" disabled="disabled">⏹️</button>
 </div>
 `;
+}
 
-	//NB: This function goes through a toString + eval by Node-RED Dashboard, so no scope
-	function initController($scope, events) {
-		const staticConfig = {STATIC:"CONFIG"};	//Placeholder for workaround, auto-updated to get our scope
+//NB: This function goes through a toString + eval by Node-RED Dashboard, so no scope
+function initController($scope, events) {
 
+	$scope.init = function (config) {
+		$scope.config = config;
 		$scope.unique = $scope.$eval('$id');
-		$scope.title = staticConfig.title || staticConfig.name || 'Upload';
+		$scope.title = config.title || config.name || 'Upload';
+		$scope.chunkCallback = null;
+		$scope.chunkWelcome = false;
+	};
 
-		function sendFile(file) {
-			const div = document.getElementById('ui-upload-' + $scope.unique);
-			const progress = div.querySelector('progress');
-			$scope.stop = false;
+	$scope.$watch('msg', function (msg) {
+		//Message received from back-end
+		if (msg && msg.payload && msg.payload.tick && !$scope.stop && !$scope.pause) {
+			if ($scope.chunkCallback) {
+				$scope.chunkCallback.f($scope.chunkCallback.e);
+			} else {
+				$scope.chunkWelcome = true;
+			}
+		}
+	});
 
-			const chunk = 1024 * Math.max(staticConfig.chunk || 1024, 1);
-			const id = file.name + ';' + file.size + ';' + Date.now();
-			const count = Math.ceil(file.size / chunk);
-			let loaded = 0;
-			let i = -1;
+	function sendFile(file) {
+		const div = document.getElementById('ui-upload-' + $scope.unique);
+		const progress = div.querySelector('progress');
+		$scope.stop = false;
+		$scope.chunkWelcome = true;
 
-			const fileReader = new FileReader();
-			fileReader.onload = function (e) {
-				i++;
-				if ($scope.stop) {
-					//Send special paquet to inform the rest of the pipeline
-					$scope.send({
-						file: {
-							lastModified: file.lastModified,
-							name: file.name,
-							size: file.size,
-							type: file.type,
-						},
-						parts: {
-							id: id,
-							type: 'string',
-							ch: '',
-							index: i,
-							count: i + 1,
-							chunk: chunk,
-							abort: true,
-						},
-						payload: '',
-					});
-					$scope.stopClick();
-					return;
-				} else if ($scope.pause) {
-					$scope.pauseCallback = { f: fileReader.onload, e: e };
-					return;
-				}
+		const chunk = 1024 * Math.max($scope.config.chunk || 1024, 1);
+		const id = file.name + ';' + file.size + ';' + Date.now();
+		const count = Math.ceil(file.size / chunk);
+		let loaded = 0;
+		let i = -1;
+
+		const fileReader = new FileReader();
+		fileReader.onload = function (e) {
+			i++;
+			if ($scope.stop) {
+				//Send special paquet to inform the rest of the pipeline
 				$scope.send({
 					file: {
 						lastModified: file.lastModified,
@@ -100,102 +94,155 @@ module.exports = function (RED) {
 						type: 'string',
 						ch: '',
 						index: i,
-						count: count,
+						count: i + 1,
 						chunk: chunk,
+						abort: true,
 					},
-					payload: e.target.result,
+					payload: '',
 				});
-				loaded += chunk;
-				progress.value = 100 * loaded / file.size;
-				if (loaded <= file.size) {
-					blob = file.slice(loaded, loaded + chunk);
-					fileReader.readAsBinaryString(blob);
-				} else {
-					loaded = file.size;
-					$scope.stopClick();
-				}
-			};
-			let blob = file.slice(0, chunk);
-			fileReader.readAsBinaryString(blob);
+				$scope.stopClick();
+				return;
+			} else if (!$scope.chunkCallback && ($scope.pause || !$scope.chunkWelcome)) {
+				$scope.chunkCallback = { f: fileReader.onload, e: e };
+				return;
+			}
+			$scope.chunkCallback = false;
+			$scope.chunkWelcome = false;
+			$scope.send({
+				file: {
+					lastModified: file.lastModified,
+					name: file.name,
+					size: file.size,
+					type: file.type,
+				},
+				parts: {
+					id: id,
+					type: 'string',
+					ch: '',
+					index: i,
+					count: count,
+					chunk: chunk,
+				},
+				payload: e.target.result,
+			});
+			loaded += chunk;
+			progress.value = 100 * loaded / file.size;
+			if (loaded <= file.size) {
+				blob = file.slice(loaded, loaded + chunk);
+				fileReader.readAsBinaryString(blob);
+			} else {
+				loaded = file.size;
+				$scope.stopClick();
+			}
+		};
+		let blob = file.slice(0, chunk);
+		fileReader.readAsBinaryString(blob);
+	}
+
+	let backgroundColor = '';
+
+	$scope.ondragleave = function (e) {
+		e.preventDefault();
+		e.stopPropagation();
+		const div = e.currentTarget;
+		div.style.background = backgroundColor;
+	};
+
+	$scope.ondragenter = function (e) {
+		e.preventDefault();
+		e.stopPropagation();
+	};
+
+	$scope.ondragover = function (e) {
+		e.preventDefault();
+		e.stopPropagation();
+		const div = e.currentTarget;
+		backgroundColor |= div.style.background;
+		div.style.background = '#55E';
+	};
+
+	$scope.ondrop = function (e) {
+		const dataTransfer = e.dataTransfer || e.originalEvent.dataTransfer;
+		if (dataTransfer && dataTransfer.files && dataTransfer.files.length > 0) {
+			e.preventDefault();
+			e.stopPropagation();
+			$scope.stopClick(e);
+			const div = e.currentTarget;
+			div.style.background = '#5E5';
+			setTimeout(function () { div.style.background = backgroundColor; }, 300);
+			const input = div.querySelector('input');
+			input.files = dataTransfer.files;
+			div.querySelector('.play').innerHTML = '⏸️';
+			div.querySelector('.play').disabled = false;
+			div.querySelector('.stop').disabled = false;
+			sendFile(dataTransfer.files[0]);
 		}
+	};
 
-		let backgroundColor = '';
-
-		$scope.ondragleave = function (e)
-		{
-			e.preventDefault();
-			e.stopPropagation();
-			const div = e.currentTarget;
-			div.style.background = backgroundColor;
-		};
-		$scope.ondragenter = function (e)
-		{
-			e.preventDefault();
-			e.stopPropagation();
-		};
-		$scope.ondragover = function (e)
-		{
-			e.preventDefault();
-			e.stopPropagation();
-			const div = e.currentTarget;
-			backgroundColor |= div.style.background;
-			div.style.background = '#55E';
-		};
-		$scope.ondrop = function (e)
-		{
-			const dataTransfer = e.dataTransfer || e.originalEvent.dataTransfer;
-			if (dataTransfer && dataTransfer.files && dataTransfer.files.length > 0) {
-				e.preventDefault();
-				e.stopPropagation();
-				$scope.stopClick(e);
-				const div = e.currentTarget;
-				div.style.background = '#5E5';
-				setTimeout(function () { div.style.background = backgroundColor; }, 300);
-				const input = div.querySelector('input');
-				input.files = dataTransfer.files;
+	$scope.playClick = function (e) {
+		const div = document.getElementById('ui-upload-' + $scope.unique);
+		if ($scope.pause) {
+			div.querySelector('.play').innerHTML = '⏸️';
+			$scope.pause = false;
+			$scope.chunkCallback.f($scope.chunkCallback.e);
+		} else if (!$scope.stop) {
+			$scope.pause = true;
+			div.querySelector('.play').innerHTML = '▶️';
+		} else {
+			$scope.stopClick(e);
+			const input = div.querySelector('input');
+			if (input.files && input.files.length > 0) {
 				div.querySelector('.play').innerHTML = '⏸️';
 				div.querySelector('.play').disabled = false;
 				div.querySelector('.stop').disabled = false;
-				sendFile(dataTransfer.files[0]);
+				sendFile(input.files[0]);
 			}
-		};
-		$scope.playClick = function (e) {
-			const div = document.getElementById('ui-upload-' + $scope.unique);
-			if ($scope.pause) {
-				div.querySelector('.play').innerHTML = '⏸️';
-				$scope.pause = false;
-				$scope.pauseCallback.f($scope.pauseCallback.e);
-				$scope.pauseCallback = null;
-			} else if (!$scope.stop) {
-				$scope.pause = true;
-				div.querySelector('.play').innerHTML = '▶️';
-			} else {
-				$scope.stopClick(e);
-				const input = div.querySelector('input');
-				if (input.files && input.files.length > 0) {
-					div.querySelector('.play').innerHTML = '⏸️';
-					div.querySelector('.play').disabled = false;
-					div.querySelector('.stop').disabled = false;
-					sendFile(input.files[0]);
+		}
+	};
+
+	$scope.stopClick = function (e) {
+		$scope.stop = true;
+		$scope.pause = false;
+		$scope.chunkWelcome = false;
+		$scope.chunkCallback = null;
+		const div = document.getElementById('ui-upload-' + $scope.unique);
+		div.querySelector('progress').value = 0;
+		div.querySelector('.play').innerHTML = '▶️';
+		div.querySelector('.play').disabled = false;
+		div.querySelector('.stop').disabled = true;
+	};
+
+	$scope.onchange = function (e) {
+		$scope.stopClick(e);
+	};
+}
+
+/**
+ * Return true if the node has any input wired to it, false otherwise.
+ */
+function nodeHasInput(node) {
+	if (node && node._flow && node._flow.global) {
+		const allNodes = node._flow.global.allNodes;
+		for (const node2Id of Object.keys(allNodes)) {
+			const node2 = allNodes[node2Id];
+			if (node2.wires) {
+				for (const wireId of Object.keys(node2.wires)) {
+					const wire = node2.wires[wireId];
+					for (const nodeId of wire) {
+						if (node.id === nodeId) {
+							return true;
+						}
+					}
 				}
 			}
-		};
-		$scope.stopClick = function (e) {
-			$scope.stop = true;
-			$scope.pause = false;
-			$scope.pauseCallback = null;
-			const div = document.getElementById('ui-upload-' + $scope.unique);
-			div.querySelector('progress').value = 0;
-			div.querySelector('.play').innerHTML = '▶️';
-			div.querySelector('.play').disabled = false;
-			div.querySelector('.stop').disabled = true;
-		};
-		$scope.onchange = function (e) {
-			$scope.stopClick(e);
-		};
+		}
 	}
+	return false;
+}
 
+module.exports = function (RED) {
 	let ui;
+
 	function uiUpload(config) {
 		const node = this;
 
@@ -205,37 +252,51 @@ module.exports = function (RED) {
 				ui = RED.require('node-red-dashboard')(RED);
 			}
 
-			RED.nodes.createNode(this, config);
+			RED.nodes.createNode(node, config);
+			const hasInput = nodeHasInput(node);
+
+			//Defined in https://github.com/node-red/node-red-dashboard/blob/39b095586bdbd517ffbce1efff35227283edda4c/index.js
 			const done = ui.addWidget({
 				node: node,
-				format: HTML,
+				format: html(config),
 				templateScope: 'local',
-				group: config.group || 0,
-				order: config.order || 0,
+				group: config.group,
+				order: config.order,
 				height: Math.max(config.height || 5, 3),
 				emitOnlyNewValues: false,
 				forwardInputMessages: false,
 				storeFrontEndInputAsState: false,
 				persistantFrontEndValue: false,
-				convertBack: function (value) {
-					return value;
+
+				//callback to prepare the message that is emitted to the front-end
+				beforeEmit: function (msg, value) {
+					return { msg: msg };
 				},
+
+				//callback to prepare the message that is sent to the output
 				beforeSend: function (msg, orig) {
+					if (!hasInput) {
+						//Send default tick for back-pressure
+						node.receive({
+							payload: {
+								tick: true,
+							},
+						});
+					}
 					if (orig) {
 						return orig.msg;
 					}
 				},
-				initController: eval(
-					//TODO: Find a better way to pass config variables to the scope,
-					//despite https://github.com/node-red/node-red-dashboard/blob/7ad96892592109c97cb6dd0447135e9d0046372c/index.js#L116
-					'(' + initController.toString().replace('{STATIC:"CONFIG"}', JSON.stringify(config)) + ')'
-				),
+
+				//callback to initialize in controller
+				initController: initController,
 			});
 			node.on('close', done);
 		}
 		catch (ex) {
-			console.log(ex);
+			console.error(ex);
 		}
 	}
+
 	RED.nodes.registerType('ui-upload', uiUpload);
 };
